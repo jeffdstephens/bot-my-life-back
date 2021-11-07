0e7dc6e186cc4036b5c9c228d5d3c93c8856e3b7
jeffdstephens <jeffdstephens@gmail.com>
2021-11-07 17:07:22 -0500

    Added flexbox layout for bots and overall site.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
-    <h1>Bot My Life Back</h1>
-    <h7>
-      human:
-      <a href="https://www.jeffdstephens.com" target="_blank">jeffdstephens</a>
-    </h7>
-    <p>This website shows how I got my life back by implementing bots and automation. These bots help me take care of some of the menial tasks throughout the day so I can do more of the things that I truly love.</p>
+    <div>
+      <h1>Bot My Life Back</h1>
+      <h7>
+        human:
+        <a href="https://www.jeffdstephens.com" target="_blank">jeffdstephens</a>
+      </h7>
+      <p>This website shows how I got my life back by implementing bots and automation. These bots help me take care of some of the menial tasks throughout the day so I can do more of the things that I truly love.</p>
+    </div>
+    <div>
+      <iframe
+        src="https://giphy.com/embed/mIZ9rPeMKefm0"
+        width="344"
+        height="480"
+        frameborder="0"
+        class="giphy-embed"
+        allowfullscreen
+      ></iframe>
+    </div>
+
+    <div class="flex-container">
+      <covid-bot-counter class="flex-item" />
+      <expense-bot-counter class="flex-item" />
+    </div>
   </div>
-  <div>
-    <iframe
-      src="https://giphy.com/embed/mIZ9rPeMKefm0"
-      width="344"
-      height="480"
-      frameborder="0"
-      class="giphy-embed"
-      allowfullscreen
-    ></iframe>
-  </div>
-  <covid-bot-counter />
-  <expense-bot-counter />
 </template>
 
 <script lang="ts">
@@ -41,6 +46,53 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1080px;
+}
+
+.flex-container {
+  display: flex;
+  padding: 20px;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.flex-item {
+  flex: 1;
+  padding: 1.5em;
+  margin: 1em;
+  text-align: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+/* Responsive layout - makes a one column layout instead of a two-column layout */
+@media (max-width: 800px) {
+  .flex-container {
+    flex-direction: column;
+  }
+}
+
+h4 {
+  text-align: left;
+}
+
+.flex-details-container {
+  display: flex;
+  padding: 0;
+  flex-direction: row;
+}
+
+.flex-label {
+  flex: 2;
+  text-align: left;
+  padding: 0.5em;
+}
+
+.flex-value {
+  flex: 1;
+  text-align: right;
+  padding: 0.5em;
 }
 </style>
--- src/components/ExpenseBotCounter.vue
+++ src/components/ExpenseBotCounter.vue
@@ -2,40 +2,64 @@
   <div class="ExpenseBotCounter">
     <h2>Monthly Cloud Services Expense Report Bot</h2>
     <div>
-      <h4>This automation collects billing information from 10 of our AWS cloud instances and creates a single expense report for submission. This bot uses UIPath and is run in attended mode each month. The time it took to complete this task manually was 25 mins. while the attended bot takes 5 mins. for a total personal time savings of 20 mins. per month.</h4>
-      <p>
-        Start date (the day my bot started doing my expense report for me):
-        <b>{{ getStartDate }}</b>
-      </p>
-      <p>
-        Today's date:
-        <b>{{ getTodaysDate }}</b>
-      </p>
-      <p>
-        Total number of months elapsed:
-        <b>{{ getDayDifferenceAndCalcs() }}</b>
-      </p>
-      <p>
-        Time to complete task (per month):
-        <b>{{expenseTaskMinutes}} mins.</b>
-      </p>
-      <p>
-        Total minutes saved:
-        <b>{{expenseMinutesElapsed}}</b>
-      </p>
-      <p>
-        Total hours saved:
-        <b>{{expenseHoursElapsed }}</b>
-      </p>
-      <p>
-        Total business
-        <b>work days saved</b>:
-        <b>{{expenseBizDaysSaved }}</b>
-      </p>
-      <h3>
-        Total DAYS I regained of my life!
-        {{expenseDaysSaved}}
-      </h3>
+      <h4>This automation collects billing information from 10 of our AWS cloud instances and creates a single expense report for submission.</h4>
+      <h4>This bot uses UIPath and is run in attended mode each month. The time it took to complete this task manually was 25 mins. while the attended bot takes 5 mins. for a total personal time savings of 20 mins. per month.</h4>
+
+      <hr />
+
+      <div class="flex-details-container">
+        <div class="flex-label">Start date (the day my bot started doing my expense report for me):</div>
+        <div class="flex-value">
+          <b>{{ getStartDate }}</b>
+        </div>
+      </div>
+      <div class="flex-details-container">
+        <div class="flex-label">Today's date:</div>
+        <div class="flex-value">
+          <b>{{ getTodaysDate }}</b>
+        </div>
+      </div>
+      <div class="flex-details-container">
+        <div class="flex-label">Total number of months elapsed:</div>
+        <div class="flex-value">
+          <b>{{ getDayDifferenceAndCalcs() }} months</b>
+        </div>
+      </div>
+      <div class="flex-details-container">
+        <div class="flex-label">Time to complete task (per month):</div>
+        <div class="flex-value">
+          <b>{{expenseTaskMinutes}} mins.</b>
+        </div>
+      </div>
+      <div class="flex-details-container">
+        <div class="flex-label">Total minutes saved:</div>
+        <div class="flex-value">
+          <b>{{expenseMinutesElapsed}} mins.</b>
+        </div>
+      </div>
+      <div class="flex-details-container">
+        <div class="flex-label">Total hours saved:</div>
+        <div class="flex-value">
+          <b>{{expenseHoursElapsed }} hrs.</b>
+        </div>
+      </div>
+      <div class="flex-details-container">
+        <div class="flex-label">
+          Total business
+          <b>work days saved</b>:
+        </div>
+        <div class="flex-value">
+          <b style="color:red;">{{expenseBizDaysSaved }} days</b>
+        </div>
+      </div>
+      <div class="flex-details-container">
+        <div class="flex-label">
+          <b>Total DAYS I regained of my life!</b>
+        </div>
+        <div class="flex-value">
+          <b style="color:red;">{{expenseDaysSaved}} days!</b>
+        </div>
+      </div>
     </div>
   </div>
 </template>
